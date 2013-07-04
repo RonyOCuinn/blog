571949b9bebe81912456560ffde2ea1991120a77
Rony O Cuinn <rocuinn@gmail.com>
2013-07-04 17:20:27 +0100

    test

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -8,3 +9,8 @@ urlpatterns = patterns('',
     url(r'^$', 'RehabLog.views.index'),
     url(r'^(?P<slug>[\w\-]+)/$', 'RehabLog.views.post'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
